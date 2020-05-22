193be9168fab8f7ca99df548b3924b521233aa16
hwb <hwb@qq.com>
2020-05-22 19:40:34 +0800

    第一次提交

---
+++ src/main/java/com/bw/Demo01.java
@@ -0,0 +1,5 @@
+package com.bw;
+
+public class Demo01 {
+
+}
---
+++ src/test/java/com/bw/Demo.java
@@ -0,0 +1,10 @@
+package com.bw;
+
+import org.junit.Test;
+
+public class Demo {
+	@Test
+	public void test01(){
+		System.out.println("今天你好吗，我很好呀");
+	}
+}
